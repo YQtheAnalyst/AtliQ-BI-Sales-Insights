86a549bdb8a41fe9c640f5e6e5ccd9a0e41e3e08
CookieMonsterxx <50974254+CookieMonsterxx@users.noreply.github.com>
2023-11-06 18:26:04 +0000

    # Q3 in data cleaning; Refine the comments

--- hoc analysis.sql
+++ hoc analysis.sql
@@ -1,5 +1,5 @@
-### Question 1 ###
-# What are the total sales in different seasons (S1 - S4) from year 2017 to 2020?
+-- Question 1
+-- What are the total sales in different seasons (S1 - S4) from year 2017 to 2020?
 SELECT
 d.year, 
 CASE
@@ -8,7 +8,7 @@ CASE
     WHEN d.month_name = "December" OR d.month_name = "January" OR d.month_name = "February" THEN "S4"
     ELSE "S1"
 END AS season,
-SUM(t.sales_qty*t.sales_amount) AS revenue
+SUM(t.sales_qty*t.norm_sales_amount) AS revenue
 FROM sales.date AS d
 RIGHT JOIN sales.transactions AS t
 ON d.date = t.order_date
@@ -17,11 +17,11 @@ ORDER BY revenue DESC
 ;
 
 
-###Question 2 ###
-# What are the sales for each product type? Which type made the highest sales revenue?
+-- Question 2
+-- What are the sales per product type? Which type made the highest sales revenue?
 SELECT 
 p.product_type,
-SUM(t.sales_qty*t.sales_amount) AS revenue
+SUM(t.sales_qty*t.norm_sales_amount) AS revenue
 FROM sales.products AS p
 RIGHT JOIN sales.transactions AS t
 ON p.product_code = t.product_code
@@ -30,22 +30,21 @@ ORDER BY revenue DESC
 ;
 
 
-###Question 3 ###
-# What are the sales for each customer? Who had the highest sales revenue? Which selling type leads to the highest sales revenue?
-SELECT * FROM sales.customers;
-SELECT * FROM sales.transactions;
+-- Question 3
+# What are the sales per customer? Who has the highest sales revenue? Which selling type has highest sales revenue?
 SELECT 
 c.customer_name,
-SUM(t.sales_qty*t.sales_amount) AS revenue
+SUM(t.sales_qty*t.norm_sales_amount) AS revenue
 FROM sales.customers AS c
 RIGHT JOIN sales.transactions AS t
 ON c.customer_code = t.customer_code
 GROUP BY c.customer_name
 ORDER BY revenue DESC
 ;
+
 SELECT 
 c.customer_type,
-SUM(t.sales_qty*t.sales_amount) AS revenue
+SUM(t.sales_qty*t.norm_sales_amount) AS revenue
 FROM sales.customers AS c
 RIGHT JOIN sales.transactions AS t
 ON c.customer_code = t.customer_code
@@ -53,22 +52,22 @@ GROUP BY c.customer_type
 ORDER BY revenue DESC
 ;
 
-###Question 4 ###
-# Which market (city) have the highest sales reveune? How about the selling situations in different zone?
-SELECT * FROM sales.markets;
-SELECT * FROM sales.transactions;
+
+-- Question 4
+-- Which market (city) has the highest sales reveune? What is the sales revenue in different zones?
 SELECT 
 m.markets_name,
-SUM(t.sales_qty*t.sales_amount) AS revenue
+SUM(t.sales_qty*t.norm_sales_amount) AS revenue
 FROM sales.markets AS m
 RIGHT JOIN sales.transactions AS t
 ON m.markets_code = t.market_code
 GROUP BY m.markets_name
 ORDER BY revenue DESC
 ;
+
 SELECT 
 m.zone,
-SUM(t.sales_qty*t.sales_amount) AS revenue
+SUM(t.sales_qty*t.norm_sales_amount) AS revenue
 FROM sales.markets AS m
 RIGHT JOIN sales.transactions AS t
 ON m.markets_code = t.market_code
@@ -77,9 +76,8 @@ ORDER BY revenue DESC
 ;
 
 
-### Question 5 ###
-# Which customer bought the most products? 
-SELECT * FROM sales.transactions;
+-- Question 5
+-- Which customer have bought the most products? 
 SELECT 
 c.customer_name,
 SUM(t.sales_qty) AS sales_qty
