631350b75a5c1d183c8ca33b26fd99eadb23c94b
YQtheAnalyst <yiqingli07@gmail.com>
2023-11-10 14:21:35 +0000

    wrangling modification

--- README.md
+++ README.md
@@ -3,53 +3,49 @@
 This project is under continuous updating...
 
 ### Introduction
-MatCore Hardware is a company which supplies computer hardware and peripherals in India, where the headquarters is located in Delhi. The sales director, Alex, faces lots of challenges including tracking the market sales in this dynamically growing market. However, the information he has is only Excel files with massive numbers, which contain information related to transaction, customers, and markets etc.. So, he approached the data analyst team with the requirements that he would like to know about the business insights in different marketing areas -- north, south and central India.
+MatCore Hardware is a company which supplies computer hardware and peripherals in India, where the headquarters is located in Delhi. The sales director, Alex, faces lots of challenges including tracking the market sales in this dynamically growing market. However, the information he has is only Excel files with massive numbers, which contain information related to transactions, customers, markets etc.. So, he approached the data analyst team with the requirements that he would like to know about the business insights in different marketing areas -- north, south and central India.
 
-This project is to analyze the sales insight in different marketing zones, visualize the key informationa, and generate report to support decision making for the sales director in MatCore. The main tool utilized are Excel, SQL and Tableau.
+This project is to analyze the sales insight in different marketing zones, visualize the key information, and generate the report to support decision-making for the sales director in MatCore. The main tools utilized are Excel, SQL and Tableau.
 
 
 ### Project Planning
 Utilized AIMS grid to organize this project.
 
-1. PURPOSE
-To uncover the sales insights for sales team to support decision making, and to automate them to reduce manal time spend in data gathering.
+1. Purpose
 
-2. STAKEHOLDES
+To uncover the sales insights for the sales team to support decision making, and to automate them to reduce manual time spent in data gathering.
+
+2. Stakeholders
+
 Sales director, marketing team, customer service team, data & analytics team and IT.
 
-3. END RESULTS
-An automated dashboard and a report provding sales insights in order to support data driven decision making.
+3. End result
 
-4. SUCCESS CRITERIA
-Dashboard(s) uncovering sales order insights with latest data available.
-Sales team able to take better decision and prove 10% cost savings of total spend.
+An automated dashboard and a report providing sales insights in order to support data-driven decision-making.
 
+4. Success critria
 
-### Identify the problems
-In order to solve the problems, I utilized 3 performance metrics to further study the sales insights -- revenue, sales growth per quarter, and average revenue per customer
+Dashboard(s) uncovering sales order insights with the latest data available.
+The sales team is able to make better decisions and prove 10% cost savings of total spend.
 
 
 ### Data cleaning and wrangling
-Firstly, I performed the data cleaning process to remove and fix inaccurate data with 4 main tasks, which included:
+Firstly, I performed the data cleaning process to remove and fix inaccurate data with 4 main tasks.
 
-(1) Identify and delete the rows where sales_amount is less than 0
-(2) Fix the typos of column names
-(3) Change all sales_amount to INR currency
-(4) Delete the Null values in the Markets table
+Secondly, I performed the data wrangling process, to create new tables for hoc analysis. I also generated 3 performance metrics in this process.
 
-Secondly, I performed the data wrangling process, where I created new columns and converted datasets into another format more suitable for further use.
-(1) Create column Revenue
-(2) Create tables containing two other performance metrics
-(3) Combine multiple tables into one more appropriate form
+Hoc analysis questions:
 
-For further information, please refer to the file (data cleaning & wrangling.sql)
-
-
-### hoc analysis
-This section focuses on preparing data to answer the following questions:
 (1) Which marketing zone has the highest revenue?
 
+(2) Which customers contribute to the highest revenue in different zones?
+
+(3) What is the sales growth per quarter from 2017 to 2022? What about it in different zones?
+
+(4) What is the average revenue per customer in different zones? What can you learn about related to customer behaviour?
+
+For detail information, please refer to the file (data cleaning & wrangling.sql)
 
 ### Data visualization
-I visualized the business insights in Tableau to uncover the hidden patternns.
+I visualized the business insights in Tableau to uncover the hidden patterns.
 For further information, please refer to the Tableau Public, 
--- data cleaning & wrangling.sql
+++ data cleaning & wrangling.sql
@@ -6,107 +6,210 @@
 -- 3. Change all sales_amount to INR currency
 -- 4. Delete the Null values in Markets table
 
--- 1. Identify and delete the rows where sales_amount are less than 0
+-- ------------------------Task 1------------------------------------
 
+-- Identify the rows where sales_amount < 0
 SELECT 
-DISTINCT sales_amount 
-FROM sales.transactions 
-ORDER BY sales_amount;
+  DISTINCT sales_amount 
+FROM 
+  sales.transactions 
+ORDER BY 
+  sales_amount;
 
-DELETE FROM sales.transactions 
-WHERE sales_amount <= 0;
+-- delete them
+DELETE FROM 
+  sales.transactions 
+WHERE 
+  sales_amount <= 0;
 
 
--- 2. fix the typo of column name in the customer table
+-- ------------------------Task 2------------------------------------
 
-ALTER TABLE sales.customers
-RENAME COLUMN custmer_name to customer_name;
+-- fix the typo of column name
+ALTER TABLE 
+  sales.customers
+RENAME COLUMN 
+  custmer_name to customer_name;
 
 
--- 3. Calculate the sales_amount which are in USD to INR (1USD ~ 83INR) (part of data wrangling)
+-- ------------------------Task 3------------------------------------
 
-SELECT DISTINCT currency FROM sales.transactions;
+SELECT DISTINCT 
+  currency 
+FROM 
+  sales.transactions;
 
 -- Change all 'INR%' to 'INR'
-UPDATE sales.transactions
-SET currency = "INR"
-WHERE currency LIKE "INR%"
+UPDATE 
+  sales.transactions
+SET 
+  currency = "INR"
+WHERE 
+  currency LIKE "INR%"
 ;
 
 -- Change all 'USD%' to 'USD'
-UPDATE sales.transactions
-SET currency = "USD"
-WHERE currency LIKE "USD%"
+UPDATE 
+  sales.transactions
+SET 
+  currency = "USD"
+WHERE 
+  currency LIKE "USD%"
 ;
 
 -- Add a new column to the existing table, for further calculation purpose
-ALTER TABLE sales.transactions
-ADD norm_sales_amount double;
+ALTER TABLE 
+  sales.transactions
+ADD 
+  norm_sales_amount double;
 
--- Calculate the sales_amount in INR unit, and store in the new column
-UPDATE sales.transactions
-SET norm_sales_amount = 
+-- for sales_amount where the currency is "USD", transform the figures to "INR"
+UPDATE 
+  sales.transactions
+SET 
+  norm_sales_amount = 
   CASE 
-	WHEN currency = "USD" THEN sales_amount*83
-	ELSE sales_amount
+	  WHEN currency = "USD" THEN sales_amount*83
+	  ELSE sales_amount
   END;
   
-  
--- 4 Delete the Null values in Markets table
-DELETE FROM sales.markets WHERE zone = "";
+
+-- ------------------------Task 4------------------------------------
+-- Delete the Null values in Markets table
+DELETE FROM 
+  sales.markets 
+WHERE 
+  zone = "";
 
 
 
+
+-- -------------------------------------------------------------------
 -- ------------------------DATA WRANGLING ----------------------------
--- There are 1 task to do in data wrangling section, generating 3 performace metrics:
--- Building the performance metrics:
--- (1) Revenue
--- (2) Sales growth per quarter
--- (3) Average revenue per customer by market zone / city
+-- Building tables present 3 performance metrics, as well as answering the following questions:
+-- 0.0 Revenue
+-- 0.1 Create the new table containing key information
+-- 1. Which marketing zone has the highest revenue?
+-- 2. Which customer contribute to the highest revenue in different zones?
+-- 3. What is the sales growth per quarter from 2017 to 2022? What about it in different zones?
+-- What is the average revenue per customer in each zone? What can you learn about related to customer behaviour?
 
--- (1) Revenue = sales Quantity * Normalized Sales Amount
+-- ------------------------Task 0.0------------------------------------
+-- Create new column where Revenue = sales Quantity * Normalized Sales Amount
+ALTER TABLE 
+  sales.transactions
+ADD 
+  revenue double;
 
-ALTER TABLE sales.transactions
-ADD revenue double;
+UPDATE 
+  sales.transactions
+SET 
+  revenue = norm_sales_amount*sales_qty;
 
-UPDATE sales.transactions
-SET revenue = norm_sales_amount*sales_qty;
 
-
--- (2) Sales growth per quarter
+-- ------------------------Task 0.1------------------------------------
+-- Combine the tables with neccessary information into one
+CREATE TABLE 
+	all_transaction
 SELECT
-d.year, 
-CASE
-	WHEN d.month_name = "June" OR d.month_name = "July" OR d.month_name = "August" OR d.month_name = "May" THEN "Q2"
-    WHEN d.month_name = "September" OR d.month_name = "October" OR d.month_name = "November" OR d.month_name = "December" THEN "Q3"
-    ELSE "Q1"
-END AS quarter,
-SUM(t.sales_qty*t.norm_sales_amount) AS revenue
-FROM sales.date AS d
-RIGHT JOIN sales.transactions AS t
-ON d.date = t.order_date
-GROUP BY d.year, quarter
-ORDER BY revenue DESC
+	c.customer_name,
+	m.markets_name,
+	m.zone,
+	t.order_date,
+	t.sales_qty,
+	t.norm_sales_amount,
+	t.revenue
+FROM 
+	sales.transactions AS t
+LEFT JOIN 
+	sales.customers AS c ON c.customer_code = t.customer_code
+LEFT JOIN 
+	sales.markets AS m ON m.markets_code = t.market_code
 ;
 
--- (3) Average revenue per customer by market zone / city
-SELECT * FROM sales.transactions;
-SELECT 
-m.zone,
-ROUND(AVG(revenue),0) AS AVG_revenue
-FROM sales.transactions AS t
-LEFT JOIN sales.markets AS m
-ON t.market_code = m.markets_code
-GROUP BY m.zone
-ORDER BY AVG_revenue DESC
+
+-- ------------------------Task 1------------------------------------
+-- Which marketing zone has the highest revenue?
+-- Calculate the total revenue in each zone/ city
+SELECT
+	zone,
+	SUM(revenue) AS total_revenue
+FROM
+	all_transaction
+GROUP BY 
+	zone
+ORDER BY 
+	revenue ASC
 ;
 
-SELECT 
-m.markets_name,
-ROUND(AVG(revenue),0) AS AVG_revenue
-FROM sales.transactions AS t
-LEFT JOIN sales.markets AS m
-ON t.market_code = m.markets_code
-GROUP BY m.markets_name
-ORDER BY AVG_revenue DESC
+SELECT
+	markets_name,
+	SUM(revenue) AS total_revenue
+FROM
+	all_transaction
+GROUP BY 
+	markets_name
+ORDER BY 
+	revenue ASC
 ;
+
+
+-- ------------------------Task 2------------------------------------
+-- Which customer contribute to the highest revenue in different zones?
+SELECT
+	zone,
+    customer_name,
+	SUM(revenue) AS total_revenue
+FROM
+	all_transaction
+GROUP BY 
+	zone, customer_name
+ORDER BY 
+	revenue ASC
+;
+
+
+-- ------------------------Task 3------------------------------------
+-- What is the sales growth per quarter from 2017 to 2022? What about it in different zones?
+CREATE TABLE 
+  sales_quarter
+SELECT
+	d.year,
+	SUM(t.revenue) AS total_revenue,
+    t.zone,
+	CASE
+		WHEN d.month_name = "June" 
+      OR d.month_name = "July" 
+      OR d.month_name = "August" 
+      OR d.month_name = "May" 
+      THEN "Q2"
+		WHEN d.month_name = "September" 
+      OR d.month_name = "October" 
+      OR d.month_name = "November" 
+      OR d.month_name = "December" 
+      THEN "Q3"
+		ELSE "Q1"
+	END AS quarter
+FROM 
+	sales.date AS d
+RIGHT JOIN 
+	sales.all_transaction AS t ON d.date = t.order_date
+GROUP BY 
+	d.year, quarter
+;
+
+
+-- ------------------------Task 4------------------------------------
+-- What is the average revenue per customer in each zone? What can you learn about related to customer behaviour?
+CREATE TABLE 
+  avg_sales_customer
+SELECT
+	zone,
+  customer_name,
+  ROUND(AVG(sales_qty),0) AS avg_sales_qty_percustomer,
+  ROUND(AVG(norm_sales_amount),0) AS avg_sales
+FROM 
+	all_transaction
+GROUP BY
+	customer_name
+;    
